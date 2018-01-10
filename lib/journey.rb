591bc831dd48b14c539a505d127d9e8edbdd1c3b
Leigh-ann <leighann.gant@gmail.com>
2018-01-10 14:25:24 +0000

    Journey now stores entry and exit station

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,7 +1,21 @@
 class Journey
-  attr_reader :entry_station
+  attr_reader :entry_station, :exit_station
+
+  def initialize
+    @complete = false
+  end
+
+  def complete?
+    @complete
+  end
 
   def start(entry_station)
     @entry_station = entry_station
   end
+
+  def complete(exit_station)
+    @exit_station = exit_station
+    @complete = true if @entry_station
+  end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -3,9 +3,27 @@ require 'journey'
 describe Journey do
   subject(:journey) { described_class.new }
   let (:entry_station) { double :entry_station }
+  let (:exit_station) { double :exit_station }
 
   it 'stores the entry station' do
     journey.start(entry_station)
     expect(subject.entry_station).to eq entry_station
+  end
+  describe '#complete' do
+    it 'stores the exit station' do
+      journey.complete(exit_station)
+      expect(journey.exit_station).to eq exit_station
+    end
+  end
+  describe '#complete?' do
+      it 'confirms journey complete when journey given a touch in and touch out station' do
+        journey.start(entry_station)
+        journey.complete(exit_station)
+        expect(journey).to be_complete
+    end
+    it 'confirms journey incomplete when only entry station provided' do
+      subject.start(entry_station)
+      expect(journey).not_to be_complete
+    end
   end
 end
